7c54da6ff1da5be1324e8191333dc6f3c7d3aaef
Andrew Cole Hoover <hoover34@data.cs.purdue.edu>
2017-09-11 16:14:36 -0400

    Compression is working. File IO needs work

--- src/compressor.c
+++ src/compressor.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
 
 int* compress(char* cleanedInput);
-char* clean(FILE *fd);
+void clean(char* input, FILE *fd);
 int countCharacters(FILE *fd);
 
 
@@ -11,7 +14,19 @@ int main(int argc, char* argv[]){
 		char* fileName = argv[1];
 		FILE *fd;
 		fd =fopen(fileName, "r");
-		printf("/s",clean(fd));
+		int charCount = countCharacters(fd);
+		char input[charCount];
+		rewind(fd);
+		clean(input, fd);
+		FILE *fo;
+		char* newFileName = strcat(fileName,".vgc");
+		if(stat(newFileName) == 0)
+			fo = fopen(newFileName,"w+");
+		else
+			fo = fopen(newFileName,"ra");
+		fprintf(fo, "%s", input);
+		int finalCharCount = countCharacters(fo);
+		printf("Initial size %d\nFinal size %d\nFile size reduced by %d %% .\n",charCount,finalCharCount, (int)((charCount-finalCharCount)/charCount)*100);
 	}
 	return 0;
 }
@@ -26,9 +41,8 @@ int countCharacters(FILE *fd){
 	return count;
 }
 
-char* clean(FILE *fd){
+void clean(char* input, FILE *fd){
 	char c;
-	char cleanedInput[countCharacters(fd)];
 	int i = 0;
 
 	while((c=fgetc(fd))!=EOF){
@@ -50,7 +64,9 @@ char* clean(FILE *fd){
 		}
 		if(c>=65 && c <=90) // TO LOWERCASE
 			c+=32;
-		if(c>=32)
-			cleanedInput[i++]=c; //Add char c to cleaned string
-	}	
+		if(c>=32 && c != 97 && c != 101 && c != 105 && c!=111 && c != 117){
+			input[i++]=c; //Add char c to cleaned string	
+		}
+	}
+	input[i]='\0';
 }
